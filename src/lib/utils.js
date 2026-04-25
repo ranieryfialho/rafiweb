88da3442dca4e80bdf1e087a90fe493d165dffb4
Raniery Fialho <ranieryfialho@gmail.com>
2026-04-25 16:14:31 -0300

    Remove prefixo antigo das imagens

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,14 +5,11 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
 
-const PLUGIN_NAME = 'rafiweb-plugin';
 const VITE_DEV_SERVER_URL = 'http://localhost:5173';
 
 export function asset(path) {
   if (import.meta.env.DEV) {
     return `${VITE_DEV_SERVER_URL}${path}`;
   }
-
-  const cleanPath = path.startsWith('/') ? path.substring(1) : path;
-  return `/wp-content/plugins/${PLUGIN_NAME}/dist/${cleanPath}`;
+  return path;
 }
